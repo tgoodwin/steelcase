9a0da96ef04671a0da0aa079632dc825d00a4f97
Tim Goodwin <timg.goodwin@gmail.com>
2023-09-28 17:34:48 -0700

    update bootstrap script

--- etcd-util/bootstrap.sh
+++ etcd-util/bootstrap.sh
@@ -2,5 +2,9 @@ CONTROL_PLANE_CONTAINER="kind-control-plane"
 
 docker exec $CONTROL_PLANE_CONTAINER /bin/sh -c "curl -LO https://github.com/etcd-io/etcd/releases/download/v3.5.0/etcd-v3.5.0-linux-amd64.tar.gz"
 docker exec $CONTROL_PLANE_CONTAINER /bin/sh -c "tar xzvf etcd-v3.5.0-linux-amd64.tar.gz"
-docker exec $CONTROL_PLANE_CONTAINER /bin/sh -c "cd etcd-v3.5.0-linux-amd64"
+docker exec $CONTROL_PLANE_CONTAINER /bin/sh -c "cd etcd-v3.5.0-linux-amd64 && mv etcd* /usr/bin"
+if [[ $(docker exec $CONTROL_PLANE_CONTAINER /bin/sh -c "which etcdctl") != "/usr/bin/etcdctl" ]]; then
+    exit 1;
+fi;
+exit 0;
 
