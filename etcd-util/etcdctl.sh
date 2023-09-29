b183003b893d1385d1c8b93a7becbeb16d332ca2
Tim Goodwin <timg.goodwin@gmail.com>
2023-09-28 17:32:25 -0700

    remote_etcdctl script

--- etcd-util/etcdctl.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-"forwards etcdctl commands to the client on the control plane node so that we can talk to the etcd cluster that kind is running"
-
-CERTPATH="/etc/kubernetes/pki/etcd"
-echo "$*"
-docker exec kind-control-plane /bin/sh -c "ETCDCTL_API=3 etcdctl --cacert $CERTPATH/ca.crt  --cert $CERTPATH/healthcheck-client.crt  --key $CERTPATH/healthcheck-client.key ${*}"
-
-
