ba05ee68bfc915bd45d36b5d86bd853eb07bb66c
Tim Goodwin <timg.goodwin@gmail.com>
2023-10-01 11:49:59 -0700

    commentgs

--- tail_knative_controller_logs.sh
+++ tail_knative_controller_logs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# tails log statements for controllers (NOT AUDIT LOGS)
 kubectl -n knative-serving logs $(kubectl -n knative-serving get pods -l app=controller -o name) -c controller -f
