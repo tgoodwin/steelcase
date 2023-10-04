b2675b6edfd563ad78513977ed5d14327276dcf8
Tim Goodwin <timg.goodwin@gmail.com>
2023-10-04 15:42:29 -0700

    add alias to pass certs to etcdctl client

--- etcd-util/bootstrap.sh
+++ etcd-util/bootstrap.sh
@@ -6,5 +6,6 @@ docker exec $CONTROL_PLANE_CONTAINER /bin/sh -c "cd etcd-v3.5.0-linux-amd64 && m
 if [[ $(docker exec $CONTROL_PLANE_CONTAINER /bin/sh -c "which etcdctl") != "/usr/bin/etcdctl" ]]; then
     exit 1;
 fi;
+docker exec $CONTROL_PLANE_CONTAINER /bin/sh -c "echo \"alias ectl='etcdctl --cacert /etc/kubernetes/pki/etcd/ca.crt --cert /etc/kubernetes/pki/apiserver-etcd-client.crt --key /etc/kubernetes/pki/apiserver-etcd-client.key'\" >> ~/.bash_aliases"
 exit 0;
 
